0d6b110707bad419ac08310d3b0a490d028d8063
Diuly Rocha <144860382+DiulyRocha@users.noreply.github.com>
2025-11-20 15:56:10 -0300

    Fix Vite manifest asset loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,24 @@
 
     <title>{{ config('app.name', 'GlowTime') }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- Carregar CSS/JS do build do Vite para produção --}}
+    @php
+        $manifestPath = public_path('build/manifest.json');
+
+        if (file_exists($manifestPath)) {
+            $manifest = json_decode(file_get_contents($manifestPath), true);
+            $cssFile = $manifest['resources/css/app.css']['file'] ?? null;
+            $jsFile = $manifest['resources/js/app.js']['file'] ?? null;
+        }
+    @endphp
+
+    @if(!empty($cssFile))
+        <link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
+    @endif
+
+    @if(!empty($jsFile))
+        <script type="module" src="{{ asset('build/' . $jsFile) }}"></script>
+    @endif
 </head>
 
 <body class="font-sans antialiased">
